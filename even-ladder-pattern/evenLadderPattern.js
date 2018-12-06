c79e2aa1a882082764cdfece4c1a51ec004b73f5
Gene83 <gene83@protonmail.com>
2018-12-06 11:13:39 -1000

    Completed evenLadderPattern

--- even-ladder-pattern/evenLadderPattern.js
+++ even-ladder-pattern/evenLadderPattern.js
@@ -1,3 +1,28 @@
 module.exports = function evenLadderPattern(num) {
-  // write your code in here
+  let ladder = ''
+  if (num === 0 || num === 1 || Math.sign(num) === -1) {
+    return ''
+  } else if( num%2 === 0) {
+    for (let i = 2; i <= num; i += 2) {
+      for (let j = 0; j < i; j++) {
+        ladder += i;
+      }
+      if( i < num ) {
+        ladder += '\n';
+      } 
+    }
+  } else if( num%2 === 1) {
+    let highestEven = num - 1
+    for (let i = 2; i <= highestEven; i += 2) {
+      for (let j = 0; j < i; j++) {
+        ladder += i;
+      }
+      if( i < highestEven ) {
+        ladder += '\n';
+      }
+    } 
+  } else {
+    return 'Not an Integer';
+  }
+  return ladder;
 }
