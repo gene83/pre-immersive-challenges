310e78f32da498844c933b9ad4827df74bb1223e
Gene83 <gene83@protonmail.com>
2018-12-06 12:57:39 -1000

    completed find-middle-value

--- find-middle-value/findMiddleValue.js
+++ find-middle-value/findMiddleValue.js
@@ -1,3 +1,13 @@
 module.exports = function findMiddleValue(numArray) {
-  // write your code in here
-}
+    let firstNum = numArray[0];
+    let secondNum = numArray[1];
+    let thirdNum = numArray[2];
+
+    if (firstNum < secondNum && secondNum < thirdNum ||  thirdNum < secondNum && secondNum < firstNum) {
+      return 1;
+    } else if (secondNum < firstNum && firstNum < thirdNum ||  thirdNum < firstNum && firstNum < secondNum) {
+      return 0;
+    } else {
+      return 2;
+    }
+ } 
