54511b4816e331235d5417e7be63093e7789149c
Gene83 <gene83@protonmail.com>
2018-12-06 00:00:53 -1000

    completed doubleStrChars

--- double-str-chars/doubleStrChars.js
+++ double-str-chars/doubleStrChars.js
@@ -1,3 +1,13 @@
 module.exports = function doubleStrChars(str) {
-  // write your code in here
+    let doubleStr = '';
+    if (typeof str === 'string') {
+        let array = str.split('');
+        for (let i = 0; i < array.length; i++) {
+        let currentChar = array[i];
+         doubleStr += (currentChar + currentChar)
+        }
+    } else {
+     return "not a string!"
+     }
+     return doubleStr
 }
