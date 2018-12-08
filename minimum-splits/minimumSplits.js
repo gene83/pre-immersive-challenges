9abb728ecffc490d239360db57b33b66d973b173
Gene83 <gene83@protonmail.com>
2018-12-07 16:48:28 -1000

    completed

--- minimum-splits/minimumSplits.js
+++ minimum-splits/minimumSplits.js
@@ -1,3 +1,7 @@
 module.exports = function minimumSplits(n, m) {
-  // write your code in here
+  if((n === 1 && m > 1) || (n > 1 && m === 1) || (n > 1 && m > 1)) {
+    return n * m - 1;
+  } else {
+    return 0;
+  }
 }
