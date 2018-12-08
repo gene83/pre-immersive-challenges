88c21623a1e924b987fc5ae48077a8e31dc2b4e4
Gene83 <gene83@protonmail.com>
2018-12-07 17:52:10 -1000

    comleted

--- move-the-zeros/moveTheZeros.js
+++ move-the-zeros/moveTheZeros.js
@@ -1,3 +1,34 @@
 module.exports = function moveTheZeros(arrNum, isRight) {
-  // write your code here
+  /* iterate through the array
+  if is true, ...
+  when value is 0, splice to remove, push to add 
+*/
+let newArr = [];
+
+if (isRight === true) {
+  let count = 0;
+  for (i=0; i < arrNum.length; i++) {
+    if (arrNum[i] != 0) {
+      newArr.push(arrNum[i]);
+    } else if (arrNum[i] === 0) {
+      count++
+    }
+  }
+  for (let i = 0; i < count; i++){
+    newArr.push(0);
+}
+} else if (isRight === false) {
+  let count = 0;
+  for (i=0; i < arrNum.length; i++) {
+    if (arrNum[i] != 0) {
+      newArr.push(arrNum[i]);
+    } else if (arrNum[i] === 0) {
+      count++
+    }
+  }
+  for (let i = 0; i < count; i++){
+    newArr.splice(0, 0, 0);
+  }
+}
+return newArr;
 }
