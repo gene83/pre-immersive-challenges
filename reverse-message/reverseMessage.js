3b22e9afe2f24beaf87f0e555cd88e4a932ba80c
Gene83 <gene83@protonmail.com>
2018-12-07 21:48:57 -1000

    not completed

--- reverse-message/reverseMessage.js
+++ reverse-message/reverseMessage.js
@@ -1,3 +1,25 @@
 module.exports = function reverseMessage(str) {
-  // write your code in here
+  /*  string lowercase
+   string.split 
+   reverse array
+   loop through and capitalize
+   join array
+   */
+
+   let lowStr = str.toLowerCase();
+   let lowArr = lowStr.split('');
+   lowArr.reverse();
+
+   if (i = 0 && lowArr[0].match(/[a-z]/i)){
+    let capLetter = lowArr[0].toUpperCase();
+    lowArr.splice(0, 1, capLetter)
+   }
+
+  for (let i = 0; i < lowArr.length; i++) {
+     if (lowArr[i] === ' ' ) {
+      let capLetter = lowArr[i+1].toUpperCase();
+       lowArr.splice(i+1, 1, capLetter)
+     }  
+  }
+  return lowArr.join('');
 }
