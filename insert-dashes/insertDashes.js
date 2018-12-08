b4775d5193a661fe205cc34309412e84c956b2a4
Gene83 <gene83@protonmail.com>
2018-12-07 16:39:29 -1000

    completed

--- insert-dashes/insertDashes.js
+++ insert-dashes/insertDashes.js
@@ -1,3 +1,15 @@
 module.exports = function insertDashes(num) {
-  // write your code in here
+  /* turn int into array of integers
+  iterate through until adjacent even numbers
+   if adjacent even, insert a dash 
+  */
+  let str = '' + num;
+  let arr = str.split('');
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i-1]%2 === 0 && arr[i]%2 === 0) {
+      arr.splice(i, 0, '-');
+    } 
+  }
+  return arr.join('');
+
 }
