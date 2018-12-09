bffcd407970b64111679f8f00415dc81252562d8
Gene83 <gene83@protonmail.com>
2018-12-09 08:23:48 -1000

    completed

--- vowel-count/vowelCount.js
+++ vowel-count/vowelCount.js
@@ -1,3 +1,9 @@
 module.exports = function vowelCount(str) {
-  // write your code in here
+  let count = 0;
+  for (let i = 0; i < str.length; i++) {
+    if ('aieouAEIOU'.includes(str[i])) {
+      count++;
+    }
+  }
+  return count;
 }
