5e15239a341a9907d7b91f0734359d3669691ee3
Gene83 <gene83@protonmail.com>
2018-12-09 09:06:39 -1000

    completed

--- swap-letter-case/swapLetterCase.js
+++ swap-letter-case/swapLetterCase.js
@@ -1,3 +1,14 @@
 module.exports = function swapLetterCase(str) {
-  // write your code in here
+  let arr = str.split('');
+  for (let i = 0; i < arr.length; i++) {
+    if ('ABCDEFGHIJKLMNOPQRSTUVWXYZ'.includes(arr[i])){
+      let lowerCase = arr[i].toLowerCase();
+      arr.splice(i, 1, lowerCase);
+    } else if ('abcdefghijklmnopqrstuvwxyz'.includes(arr[i])){
+      let upperCase = arr[i].toUpperCase();
+      arr.splice(i, 1, upperCase);
+    }
+  }
+  let swapStr = arr.join('');
+  return swapStr;
 }
